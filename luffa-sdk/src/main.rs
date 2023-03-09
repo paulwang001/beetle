5f8615f01712309cca1bbe934068468a690b646b
power wang <wangpiaoyang@snqu.com>
2023-03-09 21:29:42 +0800

    fix:bug

--- luffa-sdk/src/main.rs
+++ luffa-sdk/src/main.rs
@@ -1,7 +1,7 @@
 #![feature(poll_ready)]
 use anyhow::Result;
 use futures::pending;
-use luffa_rpc_types::{Message, message_to, ContactsToken, ContactsEvent};
+use luffa_rpc_types::{Message, message_to, ContactsToken, ContactsEvent, RtcAction};
 use luffa_sdk::{Callback, Client};
 use tracing::log::warn;
 use std::future::{Future, IntoFuture};
@@ -56,20 +56,18 @@ fn main() -> Result<()> {
         // client.send_msg(to, msg)
         println!("{:?}", peers);
         if to_id > 0 {
+
             let code = client.show_code(Some("Hello".to_owned()));
-            if let Ok((_,data)) = multibase::decode(code) {
-                let msg:Message  = serde_cbor::from_slice(&data).unwrap();
-                let msg = message_to(msg).unwrap();
-                match client.send_msg(to_id, msg) {
-                    Ok(_)=>{
-                        info!("send seccess");
-                    }
-                    Err(e)=>{
-                        error!("{e:?}");
-                    }
+            let msg = Message::WebRtc { stream_id: 0, action: luffa_rpc_types::RtcAction::Status { timestamp: 0, code } };
+            let msg = message_to(msg).unwrap();
+            match client.send_msg(to_id, msg) {
+                Ok(_)=>{
+                    info!("send seccess");
+                }
+                Err(e)=>{
+                    error!("{e:?}");
                 }
             }
-
         }
     });
 
@@ -77,24 +75,33 @@ fn main() -> Result<()> {
         let msg:Message  = serde_cbor::from_slice(&data).unwrap();
 
         match &msg {
+            Message::WebRtc { stream_id, action }=>{
+                match action {
+                    RtcAction::Status { timestamp, code }=>{
+                        if let Ok(c) = client_t.parse_contacts_code(code.clone()) {
+                            warn!("scan code: {c}");
+                            client_t.answer_contacts_code(c, Some("World".to_owned())).unwrap();
+                        }
+                    }
+                    _=>{
+
+                    }
+                }
+            }
             Message::ContactsExchange { exchange }=>{
                 match exchange {
                     ContactsEvent::Offer { token }=>{
-                        if to_id == 0 {
-                            let code = serde_cbor::to_vec(&msg).unwrap();
-                            let code = multibase::encode(multibase::Base::Base64, code);
-                            client_t.answer_contacts_code(code, Some("World".to_owned())).unwrap();
-                        }
+                        let code = serde_cbor::to_vec(&msg).unwrap();
+                        let code = multibase::encode(multibase::Base::Base64, code);
+                        client_t.answer_contacts_code(code, Some("World".to_owned())).unwrap();
                     }
                     ContactsEvent::Answer { token }=>{
-                        if to_id > 0 {
-                           let msg = Message::Chat { 
-                              content: luffa_rpc_types::ChatContent::Send { 
-                                data: luffa_rpc_types::ContentData::Text { 
-                                    source: luffa_rpc_types::DataSource::Text { content:"Ok".to_owned() }, reference: None } } } ;
-                            let msg = message_to(msg).unwrap();        
-                           client_t.send_msg(from_id, msg).unwrap();
-                        }
+                        let msg = Message::Chat { 
+                            content: luffa_rpc_types::ChatContent::Send { 
+                            data: luffa_rpc_types::ContentData::Text { 
+                                source: luffa_rpc_types::DataSource::Text { content:"Ok".to_owned() }, reference: None } } } ;
+                        let msg = message_to(msg).unwrap();        
+                        client_t.send_msg(from_id, msg).unwrap();
                     }
                 }
             }
