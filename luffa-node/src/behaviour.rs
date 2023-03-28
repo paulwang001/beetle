548347b772263f5089f1652a260e72e210f052a4
陶辰 <taochen@bugetech.com>
2023-03-28 09:48:45 +0800

    update:update_behaviour

--- luffa-node/src/behaviour.rs
+++ luffa-node/src/behaviour.rs
@@ -8,7 +8,7 @@ use cid::Cid;
 use libp2p::core::identity::Keypair;
 use libp2p::core::PeerId;
 use libp2p::gossipsub::{self, MessageAuthenticity};
-use libp2p::identify;
+use libp2p::{identify, mdns};
 use libp2p::kad::store::{MemoryStore, MemoryStoreConfig};
 use libp2p::kad::{Kademlia, KademliaConfig};
 use libp2p::mdns::tokio::Behaviour as Mdns;
@@ -90,31 +90,45 @@ impl NodeBehaviour {
         let pub_key = local_key.public();
         let peer_id = pub_key.to_peer_id();
 
-        let bitswap = if config.bitswap_client || config.bitswap_server {
-            // TODO(dig): server only mode is not implemented yet
-            let bs_config = if config.bitswap_server {
-                BitswapConfig::default()
-            } else {
-                BitswapConfig::default_client_mode()
-            };
-            tracing::warn!("init bitswap:{bs_config:?}");
-            Some(Bitswap::new(peer_id, BitswapStore(db), bs_config).await)
-        } else {
-            tracing::warn!("disabled bitswap");
-            None
-        }
-        .into();
+        let bitswap = NodeBehaviour::get_bitswap(config, peer_id, db).await;
 
-        let mdns = if config.mdns {
-            tracing::warn!("init mdns");
-            Some(Mdns::new(Default::default())?)
-        } else {
-            None
-        }
-        .into();
+        let mdns = NodeBehaviour::get_mdns(config)?;
+
+        let kad = NodeBehaviour::get_pad(config, peer_id);
+
+        let autonat = NodeBehaviour::get_autonat(config, peer_id);
+
+        let relay = NodeBehaviour::get_relay_server(config, peer_id);
+
+        let (dcutr, relay_client) = NodeBehaviour::get_relay_client(config, relay_client);
+
+        let identify = NodeBehaviour::get_identify(local_key, agent_version);
+
+        let gossipsub = NodeBehaviour::get_gossipsub(config, local_key.clone())?;
+
+        let chat = NodeBehaviour::get_chat(config, &mut peer_manager);
+        
+        Ok(NodeBehaviour {
+            ping: Ping::default(),
+            keep: libp2p::swarm::keep_alive::Behaviour::default(),
+            identify,
+            bitswap,
+            mdns,
+            chat,
+            kad,
+            autonat,
+            relay,
+            dcutr,
+            relay_client,
+            gossipsub,
+            peer_manager,
+        })
+    }
+
+    fn get_chat(config: &Libp2pConfig, peer_manager: &mut PeerManager) -> Toggle<RequestResponse<chat::ChatCodec>> {
         let cfg = libp2p::request_response::RequestResponseConfig::default();
         let protocols = iter::once((chat::ChatProtocol(), libp2p::request_response::ProtocolSupport::Full));
-        let mut chat = Some(libp2p::request_response::RequestResponse::new(chat::ChatCodec(), protocols, cfg)); 
+        let mut chat = Some(libp2p::request_response::RequestResponse::new(chat::ChatCodec(), protocols, cfg));
         chat.as_mut().map(|rq|{
             for multiaddr in &config.bootstrap_peers {
                 // TODO: move parsing into config
@@ -129,123 +143,150 @@ impl NodeBehaviour {
                 }
             }
         });
-        let kad = if config.kademlia {
-            tracing::info!("init kademlia");
-            // TODO: persist to store
-            let mem_store_config = MemoryStoreConfig {
-                max_records: 1024 * 64,
-                max_provided_keys: 1024 * 1024,
-                ..Default::default()
-            };
-            let store = MemoryStore::with_config(peer_id, mem_store_config);
-
-            // TODO: make user configurable
-            let mut kad_config = KademliaConfig::default();
-            kad_config.set_parallelism(16usize.try_into().unwrap());
-            // TODO: potentially lower (this is per query)
-            kad_config.set_query_timeout(Duration::from_secs(60));
-
-            let mut kademlia = Kademlia::with_config(pub_key.to_peer_id(), store, kad_config);
-            for multiaddr in &config.bootstrap_peers {
-                // TODO: move parsing into config
-                let mut addr = multiaddr.to_owned();
-                if let Some(Protocol::P2p(mh)) = addr.pop() {
-                    let peer_id = PeerId::from_multihash(mh).unwrap();
-                    tracing::warn!("add boot>> {:?}  {:?}",peer_id,addr);
-                    kademlia.add_address(&peer_id, addr);
-                } else {
-                    tracing::warn!("Could not parse bootstrap addr {}", multiaddr);
-                }
-            }
-
-            // Trigger initial bootstrap
-            if let Err(e) = kademlia.bootstrap() {
-                tracing::warn!("Kademlia bootstrap failed: {}", e);
-            }
-
-            Some(kademlia)
-        } else {
-            None
-        }
-        .into();
-
-        let autonat = if config.autonat {
-            tracing::info!("init autonat");
-            let pub_key = local_key.public();
-            let config = autonat::Config {
-                use_connected: true,
-                boot_delay: Duration::from_secs(0),
-                refresh_interval: Duration::from_secs(5),
-                retry_interval: Duration::from_secs(5),
-                ..Default::default()
-            }; // TODO: configurable
-            let autonat = autonat::Behaviour::new(pub_key.to_peer_id(), config);
-            Some(autonat)
-        } else {
-            None
-        }
-        .into();
-
-        let relay = if config.relay_server {
-            tracing::info!("init relay server");
-            let config = relay::v2::relay::Config::default();
-            let r = relay::v2::relay::Relay::new(local_key.public().to_peer_id(), config);
-            Some(r)
-        } else {
-            None
-        }
-        .into();
-
-        let (dcutr, relay_client) = if config.relay_client {
-            tracing::info!("init relay client");
-            let relay_client =
-                relay_client.expect("missing relay client even though it was enabled");
-            let dcutr = dcutr::behaviour::Behaviour::new();
-            (Some(dcutr), Some(relay_client))
-        } else {
-            (None, None)
-        };
-
-        let identify = {
-            let config = identify::Config::new(PROTOCOL_VERSION.into(), local_key.public())
-                .with_agent_version(format!("{}{}",agent_version.unwrap_or(String::from("luffa")),AGENT_VERSION))
-                .with_cache_size(64 * 1024);
-            identify::Behaviour::new(config)
-        };
-
-        let gossipsub = if config.gossipsub {
-            tracing::info!("init gossipsub");
-            let gossipsub_config = gossipsub::GossipsubConfigBuilder::default()
-            .validation_mode(gossipsub::ValidationMode::Strict).do_px().build().unwrap();
-            // let gossipsub_config = gossipsub::GossipsubConfig::default();
-            
-            let message_authenticity = MessageAuthenticity::Signed(local_key.clone());
-            // let message_authenticity = MessageAuthenticity::Anonymous;
-            Some(
-                gossipsub::Gossipsub::new(message_authenticity, gossipsub_config)
-                    .map_err(|e| anyhow::anyhow!("{}", e))?,
-            )
-        } else {
-            None
-        }
-        .into();
-        
-        Ok(NodeBehaviour {
-            ping: Ping::default(),
-            keep: libp2p::swarm::keep_alive::Behaviour::default(),
-            identify,
-            bitswap,
-            mdns,
-            chat:chat.into(),
-            kad,
-            autonat,
-            relay,
-            dcutr: dcutr.into(),
-            relay_client: relay_client.into(),
-            gossipsub,
-            peer_manager,
-        })
+        chat.into()
     }
+
+    /// Get a gossipsub NetworkBehaviour.
+    /// NOTE: Initialisation requires a MessageAuthenticity and GossipsubConfig instance.
+    /// If message signing is disabled,
+    /// the ValidationMode in the config should be adjusted to an appropriate level to accept unsigned messages.
+    fn get_gossipsub(config: &Libp2pConfig, local_key: Keypair) -> Result<Toggle<gossipsub::Gossipsub>> {
+        if !config.gossipsub {
+            return Ok(Toggle::from(None));
+        }
+        tracing::info!("init gossipsub");
+        let gossipsub_config = gossipsub::GossipsubConfigBuilder::default()
+            .validation_mode(gossipsub::ValidationMode::Strict).do_px().build().unwrap();
+        // let gossipsub_config = gossipsub::GossipsubConfig::default();
+
+        let message_authenticity = MessageAuthenticity::Signed(local_key);
+        // let message_authenticity = MessageAuthenticity::Anonymous;
+        Ok(Some(
+            gossipsub::Gossipsub::new(message_authenticity, gossipsub_config)
+                .map_err(|e| anyhow::anyhow!("{}", e))?,
+        ).into())
+    }
+
+    /// Get an identify NetworkBehaviour.
+    fn get_identify(local_key: &Keypair, agent_version: Option<String>) -> identify::Behaviour {
+        let config = identify::Config::new(PROTOCOL_VERSION.into(), local_key.public())
+            .with_agent_version(format!("{}{}", agent_version.unwrap_or(String::from("luffa")), AGENT_VERSION))
+            .with_cache_size(64 * 1024);
+        identify::Behaviour::new(config)
+    }
+
+    /// Get a relay NetworkBehaviour.
+    /// Relay is a NetworkBehaviour that implements the relay server functionality of the circuit relay v2 protocol.
+    fn get_relay_server(config: &Libp2pConfig, peer_id: PeerId) -> Toggle<relay::v2::relay::Relay> {
+        if !config.relay_server {
+            return Toggle::from(None);
+        }
+
+        tracing::info!("init relay server");
+        let config = relay::v2::relay::Config::default();
+        Some(relay::v2::relay::Relay::new(peer_id, config)).into()
+    }
+
+    /// Get a relay client and dcutr NetBehaviour.
+    fn get_relay_client(
+        config: &Libp2pConfig,
+        relay_client: Option<relay::v2::client::Client>,
+    ) -> (Toggle<dcutr::behaviour::Behaviour>, Toggle<relay::v2::client::Client>) {
+        if !config.relay_client {
+            return (Toggle::from(None), Toggle::from(None));
+        }
+        let relay_client =
+            relay_client.expect("missing relay client even though it was enabled");
+        tracing::info!("init relay client");
+        let dcutr = dcutr::behaviour::Behaviour::new();
+        (Some(dcutr).into(), Some(relay_client).into())
+    }
+
+    /// Get a bitswap NetworkBehaviour.
+    async fn get_bitswap(config: &Libp2pConfig, peer_id: PeerId, db: Arc<luffa_store::Store>) -> Toggle<Bitswap<BitswapStore>> {
+        if config.bitswap_client || config.bitswap_server {
+            // TODO(dig): server only mode is not implemented yet
+            let bs_config = if config.bitswap_server {
+                BitswapConfig::default()
+            } else {
+                BitswapConfig::default_client_mode()
+            };
+            tracing::warn!("init bitswap:{bs_config:?}");
+            Some(Bitswap::new(peer_id, BitswapStore(db), bs_config).await)
+        } else {
+            tracing::warn!("disabled bitswap");
+            None
+        }.into()
+    }
+
+    /// Get an mDNS NetworkBehaviour. Automatically discovers peers on the local network and adds them to the topology.
+    fn get_mdns(config: &Libp2pConfig) -> Result<Toggle<mdns::tokio::Behaviour>> {
+        if !config.mdns {
+            return Ok(Toggle::from(None));
+        }
+        tracing::warn!("init mdns");
+        Ok(Some(Mdns::new(Default::default())?).into())
+    }
+
+    /// Get an autoNAT NetworkBehaviour.
+    fn get_autonat(config: &Libp2pConfig, peer_id: PeerId) -> Toggle<autonat::Behaviour> {
+        if !config.autonat {
+            return Toggle::from(None);
+        }
+
+        tracing::info!("init autonat");
+        let config = autonat::Config {
+            use_connected: true,
+            boot_delay: Duration::from_secs(0),
+            refresh_interval: Duration::from_secs(5),
+            retry_interval: Duration::from_secs(5),
+            ..Default::default()
+        }; // TODO: configurable
+        Some(autonat::Behaviour::new(peer_id, config)).into()
+    }
+
+    /// Get a kademlia NetworkBehaviour.
+    fn get_pad(config: &Libp2pConfig, peer_id: PeerId) -> Toggle<Kademlia<MemoryStore>> {
+        if !config.kademlia {
+            return Toggle::from(None);
+        }
+
+        tracing::info!("init kademlia");
+        // TODO: persist to store
+        let mem_store_config = MemoryStoreConfig {
+            max_records: 1024 * 64,
+            max_provided_keys: 1024 * 1024,
+            ..Default::default()
+        };
+        let store = MemoryStore::with_config(peer_id, mem_store_config);
+
+        // TODO: make user configurable
+        let mut kad_config = KademliaConfig::default();
+        kad_config.set_parallelism(16usize.try_into().unwrap());
+        // TODO: potentially lower (this is per query)
+        kad_config.set_query_timeout(Duration::from_secs(60));
+
+        let mut kademlia = Kademlia::with_config(peer_id, store, kad_config);
+        for multiaddr in &config.bootstrap_peers {
+            // TODO: move parsing into config
+            let mut addr = multiaddr.to_owned();
+            if let Some(Protocol::P2p(mh)) = addr.pop() {
+                let peer_id = PeerId::from_multihash(mh).unwrap();
+                tracing::warn!("add boot>> {:?}  {:?}",peer_id,addr);
+                kademlia.add_address(&peer_id, addr);
+            } else {
+                tracing::warn!("Could not parse bootstrap addr {}", multiaddr);
+            }
+        }
+
+        // Trigger initial bootstrap
+        if let Err(e) = kademlia.bootstrap() {
+            tracing::warn!("Kademlia bootstrap failed: {}", e);
+        }
+        Some(kademlia).into()
+    }
+
+
 
     pub fn notify_new_blocks(&self, blocks: Vec<Block>) {
         if let Some(bs) = self.bitswap.as_ref() {
