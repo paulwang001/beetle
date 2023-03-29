04747a74af0702e42cb38678bfd762f522a61294
陶辰 <taochen@bugetech.com>
2023-03-29 19:56:02 +0800

    add:test

---
+++ luffa-sdk/tests/test_group_members.rs
@@ -0,0 +1,11 @@
+use image::EncodableLayout;
+use luffa_sdk::group_members::Members;
+
+#[test]
+fn test() {
+    let member  = Members::new(1, vec![1543453535,4233424]);
+    let bytes = member.to_bytes().unwrap();
+    let d = Members::deserialize(bytes.as_bytes()).unwrap();
+    assert_eq!(member.members, d.members);
+    assert_eq!(member.group_id, d.group_id);
+}
