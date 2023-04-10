02696856c305e6e1442558565738feb3bd28957c
陶辰 <taochen@bugetech.com>
2023-04-10 16:41:05 +0800

    update:group_nickname

--- luffa-sdk/src/sled_db/nickname.rs
+++ luffa-sdk/src/sled_db/nickname.rs
@@ -5,19 +5,14 @@ use crate::ClientResult;
 use crate::sled_db::contacts::{ContactsDb, KVDB_CONTACTS_TREE};
 
 pub trait Nickname: ContactsDb{
-    fn open_nickname(db: Arc<Db>) -> ClientResult<Tree> {
-        let table = format!("{}_GROUP", KVDB_CONTACTS_TREE);
-        let tree = db.open_tree(table)?;
-        Ok(tree)
-    }
 
     fn set_contacts_nickname(db: Arc<Db>, u_id: u64, nickname: &str) {
         Self::set_contacts_tag(db, u_id, nickname.to_string());
     }
 
     fn set_group_member_nickname(db: Arc<Db>, group_id: u64, u_id: u64, nickname: &str) -> ClientResult<()> {
-        let tree = Self::open_nickname(db)?;
-        let tag_key = format!("Group-{}-{}", group_id, u_id);
+        let tree = Self::open_contact_tree(db)?;
+        let tag_key = format!("TAG-{}-{}", group_id, u_id);
         println!("set group member nickname:{tag_key} ==> {}", nickname);
         tree.insert(tag_key.as_bytes(), nickname)?;
         tree.flush()?;
@@ -29,8 +24,8 @@ pub trait Nickname: ContactsDb{
     }
 
     fn get_group_member_nickname(db: Arc<Db>, group_id: u64, u_id: u64) -> ClientResult<String> {
-        let tree = Self::open_nickname(db.clone())?;
-        let tag_key = format!("Group-{}-{}", group_id, u_id);
+        let tree = Self::open_contact_tree(db.clone())?;
+        let tag_key = format!("TAG-{}-{}", group_id, u_id);
         if let Some(data) = tree.get(tag_key.as_bytes())? {
             let data = data.as_bytes();
             let nickname = String::from_utf8(data.to_vec())?;
